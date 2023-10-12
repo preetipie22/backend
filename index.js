363b1ac9b179f4d405d8e2d923792231f8b159b4
Preeti <preeti.tiwari@fibonalabs.com>
2023-10-12 18:05:19 +0530

    json added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 4000;
+
+const data = {
+  summary: [],
+  grandTotal: {
+    commercialCount: 0,
+    standbyCount: 0,
+    traineeCount: 0,
+    leaveCount: 0,
+    absentCount: 0,
+    totalCount: 0,
+  },
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -17,6 +29,10 @@ app.get("/login", (req, res) => {
 
 app.get("/youtube", (req, res) => {
   res.send("<h2>Welcome Youtube</h2>");
+});
+
+app.get("/users", (req, res) => {
+  res.json(data);
 });
 
 app.listen(process.env.PORT, () => {
